4b88736dcbb4889c7ed4fd1785f8192441d826e3
tiedsandi <fachransandi@gmail.com>
2024-04-04 20:43:56 +0700

    config full tailwind and add hero in home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
       <div className='flex-between gap-5'>
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl='/sign-in' />
         </SignedIn>
         <MobileNav />
       </div>
