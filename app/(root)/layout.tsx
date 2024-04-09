4e94528437148ee79224dae5e3751bb1a1ef91fe
tiedsandi <fachransandi@gmail.com>
2024-04-09 07:12:56 +0700

    add Toast package, add stream-io package, stream create meeting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider';
 import {ReactNode} from 'react';
 
 const RootLayout = ({children}: Readonly<{children: ReactNode}>) => {
   return (
     <main>
-      <div>{children}</div>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
     </main>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
-import {ClerkProvider} from '@clerk/nextjs';
 import {Inter} from 'next/font/google';
+import {ClerkProvider} from '@clerk/nextjs';
+import {Toaster} from '@/components/ui/toaster';
 import './globals.css';
 
 const inter = Inter({subsets: ['latin']});
@@ -30,7 +31,10 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
             colorInputText: '#fff',
           },
         }}>
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </ClerkProvider>
     </html>
   );
